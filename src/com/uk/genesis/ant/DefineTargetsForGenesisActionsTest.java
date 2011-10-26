f6a81508d3c857ee05bd1d069ea209621038f93e
Robin Datta <robindatta@gmail.com>
2011-10-26 16:31:19 +0100

    DefineTargetsForGenesisActionsTest.java: fixed Checkstyle whitespace warnings

--- src/com/uk/genesis/ant/DefineTargetsForGenesisActionsTest.java
+++ src/com/uk/genesis/ant/DefineTargetsForGenesisActionsTest.java
@@ -5,11 +5,11 @@ import org.apache.tools.ant.Project;
 import junit.framework.TestCase;
 
 public class DefineTargetsForGenesisActionsTest extends TestCase {
-	public void testDefinesCorrectActions() {
-		CreateFilterSetUsingObjects task = new CreateFilterSetUsingObjects();
-		Project project = new Project();
-		task.init();
-		task.setProject(project);
-		task.setGenesisxml("src/test/etc/genesis.xml");
-	}
+    public void testDefinesCorrectActions() {
+        CreateFilterSetUsingObjects task = new CreateFilterSetUsingObjects();
+        Project project = new Project();
+        task.init();
+        task.setProject(project);
+        task.setGenesisxml("src/test/etc/genesis.xml");
+    }
 }
