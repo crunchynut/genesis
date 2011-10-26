27e71e491bb474bd5aee24628c5fab79df0d7be5
Robin Datta <robindatta@gmail.com>
2011-10-26 17:40:13 +0100

    LoadConfig.java: fixed Checkstyle checks, regexp and some sizes warnings

--- src/com/uk/genesis/ant/LoadConfig.java
+++ src/com/uk/genesis/ant/LoadConfig.java
@@ -15,17 +15,17 @@ import org.apache.tools.ant.BuildException;
 import org.apache.tools.ant.types.FilterSet;
 
 /**
- * Creates a filterset using the provided object. This filterset can then
- * be used in various filtering operations.
+ * Creates a filterset using the provided object. This
+ * filterset can then be used in various filtering operations.
  *
- * example usage for archetype build script 
- * 
+ * example usage for archetype build script
+ *
  *    <loadconfig genesisxml="${genesis-xml}" type="Server" filtersetid="genesis.filter.properties" myPrefix="MyServer.">
  *        <object type="/Organisation/Datacentre/Rack/Server" name="/Organization/DataCentre/myRack/myServer"/>
  *    </loadconfig>
- *    
+ *
  *    <echo message="${MyServer.Server.Name}" />
- * 
+ *
  * @author Mike Mochan
  */
 public class LoadConfig extends BaseGenesisTask {
@@ -33,89 +33,109 @@ public class LoadConfig extends BaseGenesisTask {
     private String myPrefix;
     private String type;
     private List<GenesisObjectReference> objects;
-    
+
     public void init() {
         objects = new ArrayList<GenesisObjectReference>();
     }
-    
-    public void setFiltersetid(String filterSetId) {
+
+    public void setFiltersetid(final String filterSetId) {
         this.filterSetId = filterSetId;
     }
-    
-    public void addConfiguredObject(GenesisObjectReference objRef) {
+
+    public void addConfiguredObject(final GenesisObjectReference objRef) {
         if (objRef.getName() == null) {
-            throw new BuildException("property name is required on declared objects", getLocation());
+            throw new BuildException(
+                    "property name is required on declared objects",
+                     getLocation());
         }
         if (objRef.getType() == null) {
-            throw new BuildException("property type is required on declared objects", getLocation());
+            throw new BuildException(
+                    "property type is required on declared objects",
+                     getLocation());
         }
-        
+
         objects.add(objRef);
     }
-    
+
     public void execute() throws BuildException {
         validate();
-        
+
         FilterSet filterSet = new FilterSet();
-        
+
         // Add the global configuration
         try {
-            Properties globalProps = getGenesisLoader().getModelReader().getGlobalContentAsProperties();
+            Properties globalProps = getGenesisLoader()
+                    .getModelReader().getGlobalContentAsProperties();
             filterSet.addConfiguredFilterSet(getFilterForProperties(globalProps));
         } catch (IOException ex) {
-            throw new BuildException("Failed to load global configuration information", ex, getLocation());
+            throw new BuildException(
+                    "Failed to load global configuration information",
+                     ex, getLocation());
         } catch (ModelException ex) {
-            throw new BuildException("Failed to load global configuration information", ex, getLocation());
+            throw new BuildException(
+                    "Failed to load global configuration information",
+                     ex, getLocation());
         }
-        
+
         // Work through each object
         for (GenesisObjectReference ref : objects) {
             if (ref.isValid(getProject())) {
                 try {
-                    GenesisObjectType type = getGenesisLoader().getModelReader().findSingleObjectType(ref.getType());
+                    GenesisObjectType type = getGenesisLoader()
+                            .getModelReader().findSingleObjectType(ref.getType());
                     if (type == null) {
-                        throw new BuildException("Type " + ref.getType() + " is invalid", getLocation());
+                        throw new BuildException(
+                                "Type " + ref.getType() + " is invalid",
+                                 getLocation());
                     }
-                    
+
                     GenesisObject obj = type.getInstance(ref.getName());
-                    
-                    // Now add filter sets for each of the files in the hierarchy
+
+                    // Now add filter sets for each of the files in
+                    //the hierarchy
                     GenesisObject current = obj;
                     while (current != null) {
                         filterSet.addConfiguredFilterSet(getFilterForObject(current));
-                        
+
                         current = current.getParent();
                     }
                 } catch (ModelException ex) {
-                    throw new BuildException("Failed to find type " + ref.getType() + " - " + ex.getMessage(), ex, getLocation());
+                    throw new BuildException("Failed to find type "
+                            + ref.getType() + " - " + ex.getMessage(),
+                            ex, getLocation());
                 } catch (GenesisObjectNotFoundException ex) {
-                    throw new BuildException("Failed to find object " + ref.getName() + " of type " + ref.getType() + " - " + ex.getMessage(), ex, 
-                            getLocation());
+                    throw new BuildException(
+                            "Failed to find object " + ref.getName()
+                            + " of type " + ref.getType() + " - "
+                            + ex.getMessage(), ex, getLocation());
                 } catch (IOException ex) {
-                    throw new BuildException("Failed to load content for object " + ref.getName() + " of type " + ref.getType() + " - " + ex.getMessage(), ex, 
-                            getLocation());
+                    throw new BuildException("Failed to load content for object "
+                            + ref.getName() + " of type " + ref.getType() + " - "
+                            + ex.getMessage(), ex, getLocation());
                 }
             }
         }
-        
+
         getProject().addReference(filterSetId, filterSet);
     }
-    
+
     protected void validate() {
         super.validate();
-        
+
         if (filterSetId == null) {
-            throw new BuildException("property filtersetid is required", getLocation());
+            throw new BuildException(
+                    "property filtersetid is required", getLocation());
         }
     }
-    
-    protected FilterSet getFilterForObject(GenesisObject object) throws IOException {
+
+    protected FilterSet getFilterForObject(final GenesisObject object)
+            throws IOException {
         return getFilterForProperties(object.getContentAsProperties());
     }
-    
-    protected FilterSet getFilterForProperties(Properties props) {
+
+    protected FilterSet getFilterForProperties(final Properties props) {
         FilterSet result = new FilterSet();
-        
+
         //TODO Consider using the stringPropertiesName method
         Enumeration<?> propNames = props.propertyNames();
         while (propNames.hasMoreElements()) {
@@ -123,10 +143,11 @@ public class LoadConfig extends BaseGenesisTask {
             if (propName.startsWith(getType())) {
                 //System.out.println("propName = " + propName + "  Property = " + props.getProperty(propName));
                 //result.addFilter(propName, props.getProperty(propName));
-                getProject().setProperty(getMyPrefix() + propName, props.getProperty(propName));
+                getProject().setProperty(getMyPrefix() + propName,
+                                            props.getProperty(propName));
             }
         }
-        
+
         return result;
     }
 
@@ -134,7 +155,7 @@ public class LoadConfig extends BaseGenesisTask {
         return myPrefix;
     }
 
-    public void setMyPrefix(String myPrefix) {
+    public void setMyPrefix(final String myPrefix) {
         this.myPrefix = myPrefix;
     }
 
@@ -142,7 +163,7 @@ public class LoadConfig extends BaseGenesisTask {
         return type;
     }
 
-    public void setType(String type) {
+    public void setType(final String type) {
         this.type = type;
     }
 }
