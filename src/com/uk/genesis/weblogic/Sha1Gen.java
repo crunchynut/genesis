abdb1b91c6fd60f6c9adc948fd2e5b537a16390b
Robin Datta <robindatta@gmail.com>
2012-01-06 17:27:47 +0000

    Sha1Gen.java: fixed Checkstyle blocks, checks, modifier, regexp, sizes and whitespace warnings

--- src/com/uk/genesis/weblogic/Sha1Gen.java
+++ src/com/uk/genesis/weblogic/Sha1Gen.java
@@ -14,77 +14,80 @@ import java.io.*;
 import com.uk.genesis.Base64;
 
 /**
- * Generates SHA1 password required for WebLogic Portal. Accepts cleartext source password and property to store result
+ * Generates SHA1 password required for WebLogic Portal.
+ * Accepts cleartext source password and property to store result
  * @author Mike Mochan
  * @author Hussein Badakhchani
- *
  */
 public class Sha1Gen extends Task {
-	
-	private String sourcepwd;
-	private String property;
-	private String filtername;
 
-	public String getSourcepwd() {
-		return sourcepwd;
-	}
-	public void setSourcepwd(String sourcepwd) {
-		this.sourcepwd = sourcepwd;
-	}
-	public String getProperty() {
-		return property;
-	}
-	public void setProperty(String property) {
-		this.property = property;
-	}
-	
-	public String getFiltername() {
-		return filtername;
-	}
-	public void setFiltername(String filtername) {
-		this.filtername = filtername;
-	}
-	
-	public void execute() {		
-		FilterSet fs = (FilterSet) getProject().getReference(filtername);	
-	    Map<?, ?> map = fs.getFilterHash();
-    	for (Iterator<?> it = map.keySet().iterator(); it.hasNext(); ) {
-    	    Object key = it.next();
-    	    Object value = map.get(key);
-    	    
-    	    if(sourcepwd.matches(key.toString())) {
-	    	    System.out.println("Encrypting " + value + " to SHA1");
-	    	    try {
-	    	    	fs.addFilter(property , ("{SHA1}" + encrypt(value.toString())));
-	    	    }catch (NoSuchAlgorithmException nsa) {
-	    	    	throw new BuildException("No such Algorithm", getLocation());
-	    	    }
-	    	}
-    	}
-	}	 		
-    public synchronized static String encrypt(String plaintext) throws NoSuchAlgorithmException
-    {		
-	    MessageDigest md = null;
-	    try
-	    {
-	      md = MessageDigest.getInstance("SHA"); 
-	    }
-	    catch(NoSuchAlgorithmException e)
-	    {
-	      throw new NoSuchAlgorithmException(e.getMessage());
-	    }
-	    try
-	    {
-	      md.update(plaintext.getBytes("UTF-8")); 
-	    }
-	    catch(IOException e)
-	    {
-	      throw new NoSuchAlgorithmException(e.getMessage());
-	    }
+    private String sourcepwd;
+    private String property;
+    private String filtername;
 
-	    byte raw[] = md.digest(); 
-	    String hash = Base64.encodeBytes(raw); 
-	    return hash; 
-	}
+    /** @return source password */
+    public String getSourcepwd() {
+        return sourcepwd;
+    }
+
+    /** @param sourcepwd accepts a string, sets source password */
+    public void setSourcepwd(final String sourcepwd) {
+        this.sourcepwd = sourcepwd;
+    }
+
+    public String getProperty() {
+        return property;
+    }
+
+    public void setProperty(final String property) {
+        this.property = property;
+    }
+
+    public String getFiltername() {
+        return filtername;
+    }
+
+    public void setFiltername(final String filtername) {
+        this.filtername = filtername;
+    }
+
+    public void execute() {
+        FilterSet fs = (FilterSet) getProject().getReference(filtername);
+        Map<?, ?> map = fs.getFilterHash();
+        for (Iterator<?> it = map.keySet().iterator(); it.hasNext();) {
+            Object key = it.next();
+            Object value = map.get(key);
+
+            if (sourcepwd.matches(key.toString())) {
+                System.out.println("Encrypting " + value + " to SHA1");
+                try {
+                    fs.addFilter(property,
+                            ("{SHA1}" + encrypt(value.toString())));
+                } catch (NoSuchAlgorithmException nsa) {
+                    throw new BuildException("No such Algorithm", getLocation());
+                }
+            }
+        }
+    }
+
+    public static synchronized String encrypt(final String plaintext)
+            throws NoSuchAlgorithmException {
+        MessageDigest md = null;
+        try {
+            md = MessageDigest.getInstance("SHA");
+        } catch (NoSuchAlgorithmException e) {
+            throw new NoSuchAlgorithmException(e.getMessage());
+        }
+
+        try {
+            md.update(plaintext.getBytes("UTF-8"));
+        } catch (IOException e) {
+            throw new NoSuchAlgorithmException(e.getMessage());
+        }
+
+        byte[] raw = md.digest();
+        String hash = Base64.encodeBytes(raw);
+        return hash;
+    }
 
 }
