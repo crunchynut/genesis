282e3a01506b2beb1500436aaa702d50ca4ff41f
Robin Datta <robindatta@gmail.com>
2012-01-06 16:43:42 +0000

    KeystoreCopy.java: fixed Checkstyle checks, naming, regexp, and whitespace warnings

--- src/com/uk/genesis/weblogic/KeystoreCopy.java
+++ src/com/uk/genesis/weblogic/KeystoreCopy.java
@@ -16,95 +16,105 @@ import org.apache.tools.ant.BuildException;
 import org.apache.tools.ant.types.FilterSet;
 
 /**
- * Matches environment keystore files and performs copy based on src / dest / pattern match.
+ * Matches environment keystore files and performs copy based on src / dest /
+ * pattern match.
  * @author Mike Mochan
- *
  */
 public class KeystoreCopy extends Task {
-	private String message;
-	private String filtername;
-	private String destfile;	
-	private String srcfile;
+    private String message;
+    private String filtername;
+    private String destfile;
+    private String srcfile;
 
-	
-	private List<PropertyMatch> matches = new ArrayList<PropertyMatch>();
+    private List<PropertyMatch> matches = new ArrayList<PropertyMatch>();
 
-	public void execute() {
-		String munge = "";
-		
-		FilterSet fs = (FilterSet) getProject().getReference(filtername);	
-	    Map<?, ?> map = fs.getFilterHash();
-	   
-	    for (Iterator<PropertyMatch> pm = matches.iterator(); pm.hasNext();) {
-	    	PropertyMatch PatternMatch = pm.next();
-	    	Pattern GlobalPattern = Pattern.compile(PatternMatch.getPattern());
-	    	System.out.println("Matching : " + GlobalPattern.toString());
-	    	for (Iterator<?> it = map.keySet().iterator(); it.hasNext(); ) {
-	    	    Object key = it.next();
-	    	    Object value = map.get(key);
+    public void execute() {
+        String munge = "";
 
-    	    	if(GlobalPattern.matcher(key.toString()).matches()){
-    	    	    munge = value.toString();
-    	    	    StringTokenizer st = new StringTokenizer(munge, "/");    
-    	    	    String split = "";
-    	    	    for( int x = 0; st.hasMoreTokens(); x++) {
-    	    	    	split = st.nextElement().toString();
-	        	    	if ( split.endsWith("jks")) {
-	        	    		File in = new File(srcfile + "/" + split);
-	        	    		File out = new File(destfile + "/" + split);	        	    		
-	        	    		try {
-	        	    			copyFile(in, out);
-	        	    		} catch (FileNotFoundException fnf) {
-	        	    			System.out.println("keystore file " + split + " cannot be found");
-	        	    		} catch (Exception e) {
-	        	    			e.printStackTrace();
-	        	    		}
-    	    	    	}
-    	    	    }
-    	    	}
-	    	}
-	    }
-	}
-    public void copyFile(File in, File out) throws Exception {
-        FileInputStream fis  = new FileInputStream(in);
+        FilterSet fs = (FilterSet) getProject().getReference(filtername);
+        Map<?, ?> map = fs.getFilterHash();
+
+        for (Iterator<PropertyMatch> pm = matches.iterator(); pm.hasNext();) {
+            PropertyMatch patternMatch = pm.next();
+            Pattern globalPattern = Pattern.compile(patternMatch.getPattern());
+            System.out.println("Matching : " + globalPattern.toString());
+            for (Iterator<?> it = map.keySet().iterator(); it.hasNext();) {
+                Object key = it.next();
+                Object value = map.get(key);
+
+                if (globalPattern.matcher(key.toString()).matches()) {
+                    munge = value.toString();
+                    StringTokenizer st = new StringTokenizer(munge, "/");
+                    String split = "";
+                    for (int x = 0; st.hasMoreTokens(); x++ ) {
+                        split = st.nextElement().toString();
+                        if (split.endsWith("jks")) {
+                            File in = new File(srcfile + "/" + split);
+                            File out = new File(destfile + "/" + split);
+                            try {
+                                copyFile(in, out);
+                            } catch (FileNotFoundException fnf) {
+                                System.out.println("keystore file " + split
+                                        + " cannot be found");
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public void copyFile(final File in, final File out) throws Exception {
+        FileInputStream fis = new FileInputStream(in);
         FileOutputStream fos = new FileOutputStream(out);
         byte[] buf = new byte[1024];
         int i = 0;
-        while((i=fis.read(buf))!=-1) {
+        while ((i = fis.read(buf)) != - 1) {
             fos.write(buf, 0, i);
         }
         fis.close();
         fos.close();
     }
-    
-	public String getFiltername() {
-		return filtername;
-	}
-	public void setFiltername(String filtername) {
-		this.filtername = filtername;
-	}
-	public String getMessage() {
-		return message;
-	}
-	public void setMessage(String message) {
-		this.message = message;
-	}
-	public void addConfiguredMatch(PropertyMatch propMatch) {
-		if (propMatch.getPattern() == null) {
-			throw new BuildException("match must specify pattern", getLocation());
-		}
-		matches.add(propMatch);
-	}
-	public String getDestfile() {
-		return destfile;
-	}
-	public void setDestfile(String destfile) {
-		this.destfile = destfile;
-	}
-	public String getSrcfile() {
-		return srcfile;
-	}
-	public void setSrcfile(String srcfile) {
-		this.srcfile = srcfile;
-	}
+
+    public String getFiltername() {
+        return filtername;
+    }
+
+    public void setFiltername(final String filtername) {
+        this.filtername = filtername;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(final String message) {
+        this.message = message;
+    }
+
+    public void addConfiguredMatch(final PropertyMatch propMatch) {
+        if (propMatch.getPattern() == null) {
+            throw new BuildException("match must specify pattern",
+                    getLocation());
+        }
+        matches.add(propMatch);
+    }
+
+    public String getDestfile() {
+        return destfile;
+    }
+
+    public void setDestfile(final String destfile) {
+        this.destfile = destfile;
+    }
+
+    public String getSrcfile() {
+        return srcfile;
+    }
+
+    public void setSrcfile(final String srcfile) {
+        this.srcfile = srcfile;
+    }
 }
