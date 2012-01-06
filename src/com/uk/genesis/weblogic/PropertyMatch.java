c3f17d2eccf351590f2d75c751a9c28b39059173
Robin Datta <robindatta@gmail.com>
2012-01-06 16:49:25 +0000

    PropertyMatch.java: fixed Checkstyle checks, regexp, sizes and whitespace warnings

--- src/com/uk/genesis/weblogic/PropertyMatch.java
+++ src/com/uk/genesis/weblogic/PropertyMatch.java
@@ -1,22 +1,19 @@
 package com.uk.genesis.weblogic;
 
-
 /**
- * defines pattern element - Used by all internal classes to pass property from ant task.
+ * defines pattern element - Used by all internal 
+ * classes to pass property from ant task.
  * @author Mike Mochan
- *
  */
 public class PropertyMatch {
-  private String pattern;
+    private String pattern;
 
-public String getPattern() {
-	return pattern;
-}
+    public String getPattern() {
+        return pattern;
+    }
 
-public void setPattern(String pattern) {
-	this.pattern = pattern;
-}
+    public void setPattern(final String pattern) {
+        this.pattern = pattern;
+    }
 
-
-  
 }
