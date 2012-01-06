835cd02ae108efaecd3fc2264214b750609391af
Robin Datta <robindatta@gmail.com>
2012-01-06 17:40:27 +0000

    TripleDes.java: fixed Checkstyle checks, naming, regexp, sizes and whitespace warnings

--- src/com/uk/genesis/weblogic/TripleDes.java
+++ src/com/uk/genesis/weblogic/TripleDes.java
@@ -16,77 +16,83 @@ import org.apache.tools.ant.Task;
 import org.apache.tools.ant.types.FilterSet;
 
 /**
- * Generates 3DES passwords. Encrypts plaintext passwords in Archetype source.
- * Requires encryptionservice parameter specifying the WebLogic "../domain/security" directory
- * e.g. 
- *  <tripledes encryptionservice="./archetypes/wlserver92/security" filtername="genesis.filter.properties">
+ * Generates 3DES passwords. Encrypts plaintext passwords
+ * in Archetype source. Requires encryptionservice parameter
+ * specifying the WebLogic "../domain/security" directory
+ * e.g. <tripledes encryptionservice="./archetypes/wlserver92/security"
+ * filtername="genesis.filter.properties">
  * and pattern match property
- * e.g.
- * <match pattern=".*KeyPassPhrase" />  
- *    
+ * e.g. <match pattern=".*KeyPassPhrase" />  
+ *
  * @author Mike Mochan
  *
  */
 
 public class TripleDes extends Task {
-	private String encryptionservice;
-	private String filtername;
+    private String encryptionservice;
+    private String filtername;
 
-	private List<PropertyMatch> matches = new ArrayList<PropertyMatch>();
-	
-	
-	public String getEncryptionservice() {
-		return encryptionservice;
-	}
-	public void setEncryptionservice(String encryptionservice) {
-		this.encryptionservice = encryptionservice;
-	}
+    private List<PropertyMatch> matches = new ArrayList<PropertyMatch>();
 
-	public String getFiltername() {
-		return filtername;
-	}
-	public void setFiltername(String filtername) {
-		this.filtername = filtername;
-	}
-	
-	public void addConfiguredMatch(PropertyMatch propMatch) {
-		if (propMatch.getPattern() == null) {
-			throw new BuildException("match must specify pattern", getLocation());
-		}
-		matches.add(propMatch);
-	}
+    public String getEncryptionservice() {
+        return encryptionservice;
+    }
 
-	public void execute() {
-		if (getFiltername() == null) {
-			throw new BuildException("filtername must be set", getLocation());
-		}
-		if (getEncryptionservice() == null) {
-			throw new BuildException("encryptionservice must be set", getLocation());
-		}
-		if (!new File(getEncryptionservice()).exists()) {
-			throw new BuildException("encryptionservice directory must exist", getLocation());
-		}
-		
-		EncryptionService es = SerializedSystemIni.getEncryptionService(encryptionservice);
-		ClearOrEncryptedService ces = new ClearOrEncryptedService(es);
-		
-		FilterSet fs = (FilterSet) getProject().getReference(filtername);	
+    public void setEncryptionservice(final String encryptionservice) {
+        this.encryptionservice = encryptionservice;
+    }
 
-	    Map<?, ?> map = fs.getFilterHash();
-	    
-	    for (Iterator<PropertyMatch> pm = matches.iterator(); pm.hasNext();) {
-	    	PropertyMatch PatternMatch = pm.next();
-	    	Pattern GlobalPattern = Pattern.compile(PatternMatch.getPattern());
-	    //	System.out.println("Matching : " + GlobalPattern.toString());
+    public String getFiltername() {
+        return filtername;
+    }
 
-	    	for (Iterator<?> it = map.keySet().iterator(); it.hasNext(); ) {
-	    	    Object key = it.next();
-	    	    Object value = map.get(key);
+    public void setFiltername(final String filtername) {
+        this.filtername = filtername;
+    }
 
-    	    	if(GlobalPattern.matcher(key.toString()).matches()){
-    	    		fs.addFilter((key.toString() + "Encrypted"), ces.encrypt(value.toString()));
-    	    	}
-    	    }
-	    }
-	}
+    public void addConfiguredMatch(final PropertyMatch propMatch) {
+        if (propMatch.getPattern() == null) {
+            throw new BuildException("match must specify pattern",
+                    getLocation());
+        }
+        matches.add(propMatch);
+    }
+
+    public void execute() {
+        if (getFiltername() == null) {
+            throw new BuildException("filtername must be set", getLocation());
+        }
+        if (getEncryptionservice() == null) {
+            throw new BuildException("encryptionservice must be set",
+                    getLocation());
+        }
+        if (! new File(getEncryptionservice()).exists()) {
+            throw new BuildException("encryptionservice directory must exist",
+                    getLocation());
+        }
+
+        EncryptionService es = SerializedSystemIni
+                .getEncryptionService(encryptionservice);
+        ClearOrEncryptedService ces = new ClearOrEncryptedService(es);
+
+        FilterSet fs = (FilterSet) getProject().getReference(filtername);
+
+        Map<?, ?> map = fs.getFilterHash();
+
+        for (Iterator<PropertyMatch> pm = matches.iterator(); pm.hasNext();) {
+            PropertyMatch patternMatch = pm.next();
+            Pattern globalPattern = Pattern.compile(patternMatch.getPattern());
+            // System.out.println("Matching : " + globalPattern.toString());
+
+            for (Iterator<?> it = map.keySet().iterator(); it.hasNext();) {
+                Object key = it.next();
+                Object value = map.get(key);
+
+                if (globalPattern.matcher(key.toString()).matches()) {
+                    fs.addFilter((key.toString() + "Encrypted"),
+                            ces.encrypt(value.toString()));
+                }
+            }
+        }
+    }
 }
