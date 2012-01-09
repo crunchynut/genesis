6270282242efe67559224269901c3ba1401ac9b9
Robin Datta <robindatta@gmail.com>
2012-01-09 17:19:12 +0000

    GenesisArchetypeOperation.java: fixed Checkstyle regexp, sizes and whitespace warnings

--- src/com/uk/genesis/archetype/GenesisArchetypeOperation.java
+++ src/com/uk/genesis/archetype/GenesisArchetypeOperation.java
@@ -1,41 +1,41 @@
 package com.uk.genesis.archetype;
 
 /**
- * Data class providing details of an operation that can occur on an
- * archetype.
- * 
+ * Data class providing details of an operation
+ * that can occur on an archetype.
  * @author paul.jones
  */
 public class GenesisArchetypeOperation {
-	/**
-	 * The name of the operation.
-	 */
-	private final String name;
-	
-	/**
-	 * The target to invoke with the operation.
-	 */
-	private final String target;
+    /**
+     * The name of the operation.
+     */
+    private final String name;
 
-	public GenesisArchetypeOperation(final String name, final String target) {
-		super();
-		this.name = name;
-		this.target = target;
-	}
+    /**
+     * The target to invoke with the operation.
+     */
+    private final String target;
 
-	/**
-	 * Retrieves the name of the operation.
-	 * @return the name.
-	 */
-	public String getName() {
-		return name;
-	}
+    public GenesisArchetypeOperation(final String name,
+                                    final String target) {
+        super();
+        this.name = name;
+        this.target = target;
+    }
 
-	/**
-	 * Retrieves the target of the operation.
-	 * @return the target.
-	 */
-	public String getTarget() {
-		return target;
-	}
+    /**
+     * Retrieves the name of the operation.
+     * @return the name.
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Retrieves the target of the operation.
+     * @return the target.
+     */
+    public String getTarget() {
+        return target;
+    }
 }
