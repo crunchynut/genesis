cc3ebc1307948e383cf0e1ed01c869099a7e454a
Robin Datta <robindatta@gmail.com>
2012-01-09 13:40:55 +0000

    ArchetypeFactory.java: fixed Checkstyle regexp and whitespace warnings

--- src/com/uk/genesis/archetype/ArchetypeFactory.java
+++ src/com/uk/genesis/archetype/ArchetypeFactory.java
@@ -1,14 +1,14 @@
 package com.uk.genesis.archetype;
 
 /**
- * Provides the ability to work with archetypes defined in the genesis.xml file.
- * 
+ * Provides the ability to work with archetypes
+ * defined in the genesis.xml file.
  * @author paul.jones
  */
 public interface ArchetypeFactory {
-	/**
-	 * Retrieves all of the defined archetypes.
-	 * @return the actions.
-	 */
-	GenesisArchetype[] getArchetypes();
+    /**
+     * Retrieves all of the defined archetypes.
+     * @return the actions.
+     */
+    GenesisArchetype[] getArchetypes();
 }
