0c55f6d5193fde3e290c0860d89c0bd5c43cbf59
Robin Datta <robindatta@gmail.com>
2012-01-09 17:27:53 +0000

    GenesisArchetypeRequirement.java: fixed Checkstyle checks, javadoc, regexp, sizes and whitespace warnings

--- src/com/uk/genesis/archetype/GenesisArchetypeRequirement.java
+++ src/com/uk/genesis/archetype/GenesisArchetypeRequirement.java
@@ -7,54 +7,59 @@ import com.uk.genesis.model.GenesisObjectType;
  * @author paul.jones
  */
 public class GenesisArchetypeRequirement {
-	/**
-	 * The type that the action requires.
-	 */
-	private GenesisObjectType type;
-	
-	/**
-	 * The property to expose the qualified object name under.
-	 */
-	private String qnameProperty;
-	
-	/**
-	 * The property to expose the simple object name under.
-	 */
-	private String nameProperty;
-	
-	/**
-	 * Creates a new requirement for the given type and exposed property.
-	 * @param type the type that  the action requires.
-	 * @param qNameProperty the property that the qualified object name will be exposed under.
-	 * @param nameProperty the property that the local object name will be exposed under.
-	 */
-	public GenesisArchetypeRequirement(GenesisObjectType type, String qnameProperty, String nameProperty) {
-		this.type = type;
-		this.qnameProperty = qnameProperty;
-		this.nameProperty = nameProperty;
-	}
+    /**
+     * The type that the action requires.
+     */
+    private GenesisObjectType type;
 
-	/**
-	 * Retrieves the type that the action requires.
-	 * @return the required type.
-	 */
-  public GenesisObjectType getType() {
-    return type;
-  }
-	
-  /**
-   * Retrieves the name of the property that the qualified object instance will be exposed under.
-   * @return the name of the property.
-   */
-  public String getQNameProperty() {
-    return qnameProperty;
-  }
+    /**
+     * The property to expose the qualified object name under.
+     */
+    private String qnameProperty;
 
-  /**
-   * Retrieves the name of the property that the local object name will be exposed under.
-   * @return the name of the property.
-   */
-  public String getNameProperty() {
-    return nameProperty;
-  }
+    /**
+     * The property to expose the simple object name under.
+     */
+    private String nameProperty;
+
+    /**
+     * Creates a new requirement for the given type and exposed property.
+     * @param type the type that the action requires.
+     * @param qnameProperty the property that
+     * the qualified object name will be exposed under.
+     * @param nameProperty the property that the
+     * local object name will be exposed under.
+     */
+    public GenesisArchetypeRequirement(final GenesisObjectType type,
+            final String qnameProperty, final String nameProperty) {
+        this.type = type;
+        this.qnameProperty = qnameProperty;
+        this.nameProperty = nameProperty;
+    }
+
+    /**
+     * Retrieves the type that the action requires.
+     * @return the required type.
+     */
+    public GenesisObjectType getType() {
+        return type;
+    }
+
+    /**
+     * Retrieves the name of the property that the
+     * qualified object instance will be exposed under.
+     * @return the name of the property.
+     */
+    public String getQNameProperty() {
+        return qnameProperty;
+    }
+
+    /**
+     * Retrieves the name of the property that the
+     * local object name will be exposed under.
+     * @return the name of the property.
+     */
+    public String getNameProperty() {
+        return nameProperty;
+    }
 }
